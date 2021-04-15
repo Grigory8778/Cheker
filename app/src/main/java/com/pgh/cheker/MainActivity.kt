e773b503cd8c62dee41d573c71c096d7d9d0335e
Grigory8778 <77728835+Grigory8778@users.noreply.github.com>
2021-04-15 12:03:59 +0300

    Добавил меню сделал исправления

--- app/src/main/java/com/pgh/cheker/MainActivity.kt
+++ app/src/main/java/com/pgh/cheker/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.pgh.cheker
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar=findViewById<Toolbar>(R.id.tobar)
+       setSupportActionBar(toolbar)
     }
-}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main , menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    }
